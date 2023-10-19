6c69660e94f4b67804fc2bcf56d219f3b3df2488
Matthew <+>
2023-10-19 18:36:37 +0300

    dom modifying element

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(header1);
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = " YOUR-NAME is the champion";
